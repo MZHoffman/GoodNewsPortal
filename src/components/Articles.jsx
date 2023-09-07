724c9f94f955281cea3347064ab74adc9b9cc740
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-09-07 14:53:13 +0100

    filtering by topic

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -4,18 +4,22 @@ import { Grid } from '@mui/material';
 import { getArticles } from '../__utils__/api';
 
 import ArticleCard from './ArticleCard';
+import DataController from './DataController';
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
+  const [params, setParams] = useState([]);
+  console.log('🚀 ~ Articles ~ params:', { params });
 
   useEffect(() => {
-    getArticles().then(({ data }) => {
+    getArticles(params).then(({ data }) => {
       setArticles(data.articles);
     });
-  }, []);
+  }, [params]);
 
   return (
     <main>
+      <DataController params={params} setParams={setParams} />
       <Grid container spacing={2}>
         {articles.map((article) => (
           <ArticleCard key={article.article_id} article={article} />
